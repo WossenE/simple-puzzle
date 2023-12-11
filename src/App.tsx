b39aa124873b1646c7e403c31e966ac76a8031da
WossenE <w.hailemariam11@gmail.com>
2023-12-11 23:49:46 +0100

    customize input

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 // App.tsx
-import './App.css';
-import Puzzle from './SlidePuzzle';
+import { useState } from "react";
+import "./App.css";
+import SlidePuzzle from "./SlidePuzzle";
+import RowsColsInput from "./RowsColsInput";
 
 function App() {
+  const [rowsCols, setRowsCols] = useState<number | undefined>();
   return (
     <div className="App">
       <header className="App-header">
-  
         <h1>Puzzle Challenge</h1>
-        <Puzzle />
+        {!rowsCols ? (
+          <RowsColsInput setRowsCols={setRowsCols} />
+        ) : (
+          <SlidePuzzle rowsCols={rowsCols} />
+        )}
       </header>
     </div>
   );
